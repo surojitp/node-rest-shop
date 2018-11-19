45cf5a07e85a57dddf127ac324c903cd899d2e70
unknown <rocksuro007@gmail.com>
2018-11-19 09:56:03 +0530

    changes made on app.js for cross-origin

--- app.js
+++ app.js
@@ -20,6 +20,21 @@ var app = express();
 
 //////
 
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header(
+        'Access-Control-Allow-Headers','*'
+    );
+    if(req.method === "OPTIONS"){
+        res.header(
+            'Access-Control-Allow-Method',
+            'GET, POST, PUT, DELETE, PATCH'
+        )
+        return res.status(200).json({})
+    }
+    
+})
+
 //////////db///
 
 mongoose.connect('mongodb://admin:admin1234@ds045157.mlab.com:45157/nodeshop',
