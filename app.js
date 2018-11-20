21424a6961bdf68455556c5bcbae3a0b39ad7455
unknown <rocksuro007@gmail.com>
2018-11-20 18:57:17 +0530

    changes 8 made on app.js for cross-origin changed header 13

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ app.use((req,res,next)=>{
   // // Pass to next layer of middleware
   // next();
 
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://restshopingcart.herokuapp.com");
   res.header("Access-Control-Allow-Headers", "*");
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   next();
