6d7baa8a9e6746ef736b03e5c0d3373d6c0b71a3
unknown <rocksuro007@gmail.com>
2018-11-20 18:43:09 +0530

    changes 8 made on app.js for cross-origin changed header 10

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.use((req,res,next)=>{
   // next();
 
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Headers", "*");
   next();
   
 })
