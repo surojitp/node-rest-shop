1aecfc0c82a04a93d66834f2a48fbbb35c5ea9b6
unknown <rocksuro007@gmail.com>
2018-12-30 11:17:00 +0530

    changing on user file for register error message 6

--- api/controller/user.js
+++ api/controller/user.js
@@ -15,7 +15,7 @@ exports.signup =  (req,res,next) =>{
                 // return res.status(409).json({
                 //     message: "Email already use"
                 // })
-                res.status(409).send({status:409, message: 'internal error', type:'internal'}); 
+                return res.status(409).send({status:409, message: 'internal error', type:'internal'}); 
             }else{
 
                 
