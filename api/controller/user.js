ea4e2989ceb5428b25fce700c36614e7b311cf56
unknown <rocksuro007@gmail.com>
2018-12-30 11:26:46 +0530

    changing on user file for register error message 8

--- api/controller/user.js
+++ api/controller/user.js
@@ -85,10 +85,11 @@ exports.signup =  (req,res,next) =>{
             }
         })
         .catch(err=>{
-            console.log(err)
-            res.status(500).json({
-                error: err
-            })
+            // console.log(err)
+            // res.status(500).json({
+            //     error: err
+            // })
+            throw err;
         })    
 
 }
