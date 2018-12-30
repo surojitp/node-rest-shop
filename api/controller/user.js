d88cc7ef4d4883bf8168b62e27ba167bfedb70ac
unknown <rocksuro007@gmail.com>
2018-12-30 10:57:44 +0530

    changing on user file for register error message 3

--- api/controller/user.js
+++ api/controller/user.js
@@ -15,7 +15,11 @@ exports.signup =  (req,res,next) =>{
                 // return res.status(409).json({
                 //     message: "Email already use"
                 // })
-                return res.status(200).send('Email already use');
+                res.status(err.status || 409);
+                return res.render('error', {
+                    message: "email used", 
+                    error: err
+                });
             }else{
 
                 
